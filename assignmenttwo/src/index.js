0ccdfd37f4ea333f129f309235a4b9665d43ac30
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-23 16:29:55 +0000

    Set up state for customer name

--- assignmenttwo/src/index.js
+++ assignmenttwo/src/index.js
@@ -1,4 +1,5 @@
 import react from "react";
+import { useState } from "react";
 import reactdom from "react-dom";
 import {
   ChakraProvider,
@@ -8,7 +9,11 @@ import {
   Grid,
   SimpleGrid,
   Flex,
+  HStack,
+  VStack,
   GridItem,
+  Input,
+  Divider,
   Stack,
   Spacer,
   Center,
@@ -18,27 +23,68 @@ import {
   FormLabel,
 } from "@chakra-ui/react";
 
-reactdom.render(
-  <ChakraProvider>
+function App() {
+  const [name, setName] = useState("");
+  return (
     <div style={{ padding: "10px" }}>
       <Flex marginBottom="5">
         <Box w="100%" p="4" bg="red.400">
-          Pure Taq
+          <Center>
+            <Heading>Pure Taqueria</Heading>
+          </Center>
         </Box>
       </Flex>
       <SimpleGrid marginBottom="20px" spacing="20px" minChildWidth="400px">
         <Box bg="tomato" p="4">
-          Order Menu
+          <Center>
+            <Heading size="md">Order Menu</Heading>
+          </Center>
+          <FormLabel>Name for Order:</FormLabel>
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+          />
         </Box>
         <Box bg="tomato" p="4">
-          Recipt
+          <Center m="5">
+            <VStack>
+              <Heading size="md">Pure Taqueria Receipt</Heading>
+              <Divider w="100%" />
+              <Text>103 Roswell Street</Text>
+              <Text>Alpharetta, GA</Text>
+              <Text>30009</Text>
+              <Text>(678)-240-0023</Text>
+              <Divider />
+              <Text>Customer Name: {name}</Text>
+            </VStack>
+          </Center>
+          <Divider />
+          <HStack>
+            <Text>Item</Text>
+            <Spacer />
+            <Text>Cost </Text>
+          </HStack>
+          <Divider />
+          <HStack>
+            <Text>Total</Text>
+            <Spacer />
+            <Text>Cost</Text>
+          </HStack>
         </Box>
       </SimpleGrid>
-
-      <Box pos="sticky" bottom="0" left="0" w="100%" p="4" bg="red.400">
-        Not really Pure 2021
+      <Box w="100%" p="4" bg="red.400">
+        <Center>
+          <Text>Pure Taqueria est. 1921</Text>
+        </Center>
       </Box>
     </div>
+  );
+}
+
+reactdom.render(
+  <ChakraProvider>
+    <App />
   </ChakraProvider>,
   document.getElementById("root")
 );
