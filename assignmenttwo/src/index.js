38bd91f6b8e250f36209ebf7d01c2e15f4cffe4e
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-23 15:42:27 +0000

    simple layout done

--- assignmenttwo/src/index.js
+++ assignmenttwo/src/index.js
@@ -5,20 +5,39 @@ import {
   Heading,
   Text,
   Link,
+  Grid,
+  SimpleGrid,
+  Flex,
+  GridItem,
+  Stack,
+  Spacer,
   Center,
+  Radio,
+  RadioGroup,
+  Box,
   FormLabel,
 } from "@chakra-ui/react";
 
 reactdom.render(
   <ChakraProvider>
-    <div>
-      <Center>
-        <Heading>Gummy World</Heading>
-      </Center>
-      <form>
-        <FormLabel>Name:</FormLabel>
-        <input type="text" />
-      </form>
+    <div style={{ padding: "10px" }}>
+      <Flex marginBottom="5">
+        <Box w="100%" p="4" bg="red.400">
+          Pure Taq
+        </Box>
+      </Flex>
+      <SimpleGrid marginBottom="20px" spacing="20px" minChildWidth="400px">
+        <Box bg="tomato" p="4">
+          Order Menu
+        </Box>
+        <Box bg="tomato" p="4">
+          Recipt
+        </Box>
+      </SimpleGrid>
+
+      <Box pos="sticky" bottom="0" left="0" w="100%" p="4" bg="red.400">
+        Not really Pure 2021
+      </Box>
     </div>
   </ChakraProvider>,
   document.getElementById("root")
