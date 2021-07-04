0ee300fd650c242af2dfc69de813d86507d78841
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-04 15:49:58 +0000

    this array shit is messing up

--- checkers/index.js
+++ checkers/index.js
@@ -1,21 +1,25 @@
 var checkerboard = new Array(8).fill(new Array(8));
 
 for (let index = 0; index < 8; index++) {
+  console.log(index);
+  console.log("index mod" + (index % 2));
   for (let subindex = 0; subindex < 8; subindex++) {
-    if (index % 2 === 0) {
-      if (subindex % 2 === 0) {
+    if (index % 2 == 0) {
+      if (subindex % 2 == 0) {
+        console.log(true);
         checkerboard[index][subindex] = "black";
-      } else {
+      } else if (subindex == 1) {
         checkerboard[index][subindex] = "white";
-      }
-      console.log(index);
-    } else {
-      if (subindex % 2 === 0) {
-        checkerboard[index][subindex] = "white";
-      } else {
-        checkerboard[index][subindex] = "black";
       }
     }
+
+    // if (index % 2 == 1) {
+    //   if (subindex % 2 === 0) {
+    //     checkerboard[index][subindex] = "white";
+    //   } else if (subindex == 1) {
+    //     checkerboard[index][subindex] = "black";
+    //   }
+    // }
   }
 }
 
